8e416f8cb208cc66cd8047fdc4b0f48895425560
Vaibhav776 <43270071+Vaibhav776@users.noreply.github.com>
2023-06-26 11:39:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         Learn React From Nugen It Service By Varun Sharma
+         Learn React 
         </a>
       </header>
     </div>
